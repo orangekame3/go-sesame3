7c73b268869c9ed7267d8447ead65b607e508ce5
orangekame3 <miya.org.0309@outlook.jp>
2021-08-21 17:02:08 +0900

    update

--- export/go.mod
+++ export/go.mod
@@ -1,3 +1,3 @@
-module github.com/go-sesame/test/export
+module github.com/go-sesame3/export
 
 go 1.16
--- main.py
+++ main.py
@@ -1,4 +1,3 @@
-#-*= coding: utf-8 -*-
 from ctypes import *
 import ctypes
 import datetime
@@ -7,36 +6,72 @@ from Crypto.Cipher import AES
 import struct
 import os
 from dotenv import load_dotenv
-#import nfc_connect
-#import subprocessa
+# import nfc_connect
+# import subprocess
 
-class go_string(Structure):
-    _fields_ = [("p", c_char_p),("n", c_int)]
-#GO言語用string変換
-
-def GoString(s):
-    u=s.encode('utf-8')
-    return go_string(c_char_p(u), len(u))
-
-
-print(os.environ['HOME'])
+# 環境変数の読み込みにはpython-dotenvを使用している
+# 事故が起きないように.envファイルはhomeディレクトリ配下に置く
 HOME = os.environ['HOME']
 load_dotenv(HOME+'/.env')
 SECRET_KEY = os.environ["SECRET_KEY"]
 API_TOKEN = os.environ["API_TOKEN"]
 UUID = os.environ["UUID"]
+ANDROIDO = os.environ["ANDROIDO"].encode()
+SUICA = os.environ["SUICA"].encode()
 
+
+class MySesame3:
+    '''docstring
+    ・セサミ3のクラス、libはgoでビルドしたバイナリファイルを読み込んでいる
+    ・API_TOKENはこちら(https://dash.candyhouse.co/login)で取得する
+    ・UUIDはアプリに記載されている対象のセサミ3のUUID
+    ・SECRET_KEYはアプリを立ち上げて「鍵のシェア（オーナー）」で生成されるQRコードを読み込んで得られる文字列
+    ・施錠/解錠の際はSECRET_KEYとタイムスタンプをAES-CMACによって暗号化する必要がある(encryptmyKey)
+    ・作成した暗号キーをsignとしてAPI_TOKEN,UUIDとともにPOSTすることで施錠解錠ができる(lockOrunlock)
+    ・現在の鍵の状態(施錠中/解除中)はgolang側で判断する
+    ・施錠中であれば解錠コマンドを、解錠中であれば施錠コマンドを打ち込む
+    '''
+    def __init__(self):
+        self.lib = cdll.LoadLibrary("./export/export.so")
+        self.key = SECRET_KEY
+        self.api = API_TOKEN
+        self.uuid = UUID
+        self.sign = ""
+
+    def encyptmyKey(self):
+        timestamp = int(datetime.datetime.now().timestamp()).to_bytes(4, 'little', signed=False)[1:4]
+        cmac = CMAC.new(bytes.fromhex(self.key), ciphermod=AES)
+        cmac.update(timestamp)
+        self.sign =  cmac.hexdigest()
+
+    def lockOrunlock(self):
+        self.lib.executeSesame3.restype=c_char_p
+        self.lib.executeSesame3(self.sign.encode('utf-8'),self.api.encode('utf-8'),self.uuid.encode('utf-8'))
+
+# ANDROIDO =b'01407fc7d137b660'
+def ismyID(id):
+    return bool(id==ANDROIDO or id ==SUICA)
+
+detectedID  =b'01010112be1aff08'
 
 # if __name__ == '__host__':
-#     while True:
-#         ID = nfc_connect.detect()
-#         subprocess.call("aplay ic.wav" ,shell=True)
-#         if ID == b'01010112be1aff08' or b'01407fc7d137b660':
-message = int(datetime.datetime.now().timestamp()).to_bytes(4, 'little', signed=False)[1:4]
-cmac = CMAC.new(bytes.fromhex(SECRET_KEY), ciphermod=AES)
-cmac.update(message)
-SIGN = cmac.hexdigest()
-#パラメータの準備
-lib = cdll.LoadLibrary("./export/export.so")
-lib.operation.restype=c_char_p
-lib.operation(GoString(SIGN),GoString(API_TOKEN),GoString(UUID),)
+#      while True:
+#         # nfcpyによるNFC入力待機
+#         #detectedID = nfc_connect.detect()
+#         # NFCの入力を検知したらスピーカーから通知音を出す
+#         #subprocess.call("aplay ic.wav" ,shell=True)
+#         if ismyID(detectedID):
+#         # セサミ3インスタンスの作成
+#             mySesame3 = MySesame3()
+#         # secret_keyを暗号化
+#             mySesame3.encyptmyKey()
+#         # 施錠と解錠の実行
+#             mySesame3.lockOrunlock()
+
+mySesame3 = MySesame3()
+# secret_keyを暗号化
+mySesame3.encyptmyKey()
+# 施錠と解錠の実行
+mySesame3.lockOrunlock()
+
+            
